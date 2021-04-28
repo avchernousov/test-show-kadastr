b0dc57c47cee3b9bad004594df25cf28d3819834
Alim Liumanov <alm.lyumanov@gmail.com>
2021-04-28 15:46:18 +0300

    ZBPRESALE-1211, added cors

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 import json
+import logging
 import os
 
-from aiohttp import web, ClientResponseError
+import aiohttp_cors as aiohttp_cors
+from aiohttp import web
 from rosreestr2coord import Area
 
 
 async def get_poly(request) -> web.Response:
-    cadastr_id = request.query['cadastr_id']
+    print(10 * '!')
+    try:
+        cadastr_id = request.query['cadastr_id']
+    except KeyError as e:
+        return web.json_response({"error": "Incorrect parameter"})
     area = Area(cadastr_id)
 
     data = area.to_geojson_poly()
@@ -27,8 +33,24 @@ async def get_ids(request) -> web.Response:
 
 async def init_app() -> web.Application:
     app = web.Application()
-    app.router.add_route('GET', '/get_poly', get_poly)
-    app.router.add_route('GET', '/get_ids', get_ids)
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(),
+    })
+
+    ids_resource = cors.add(app.router.add_resource("/get_ids"))
+    cors.add(ids_resource.add_route("GET", get_ids), {
+        "*": aiohttp_cors.ResourceOptions(expose_headers="*",
+                                          allow_headers="*",
+                                          allow_credentials=True, ),
+    })
+    cadastr_info = cors.add(app.router.add_resource("/get_poly"))
+    cors.add(cadastr_info.add_route("GET", get_poly), {
+        "*": aiohttp_cors.ResourceOptions(expose_headers="*",
+                                          allow_headers="*",
+                                          allow_credentials=True, ),
+    })
+
+    logging.basicConfig()
     return app
 
 
