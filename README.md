aaaa07338a03a285f35cf1e2cc34a0ac8772f052
Maria <tmsdesign@yandex.ru>
2021-04-28 19:15:48 +0300

    Center added to map (#6)
    * Center fixed and build
    
    * new build
    
    * Hotfixes
    
    * Readme changed
    
    * deleted zip
    
    * Readme fixies

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # test-show-kadastr
 Отображение на карте полигона объекта, полученного по кадастровому номеру
 
+### сборка фронтенда
+cd ./front
+yarn install
+yarn build
 ## скачиваем библиотеку
 git clone https://github.com/rendrom/rosreestr2coord 
 ## переходим в нее
--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [ options, setOptions ] = useState([]);
   const [ type, setType ] = useState('');
   const [ coords, setCoords ] = useState([]);
+  const [center, setCenter ] = useState({});
 
   useEffect(() => {
     fetch('/get_ids')
@@ -20,6 +21,7 @@ const App = () => {
       .then((data) => {
         setType(data.features[0].geometry.type);
         setCoords(data.features[0].geometry.coordinates[0]);
+        setCenter(data.features[0].properties.center);
       })
       .catch((error) => console.log(error));
   }
@@ -27,7 +29,10 @@ const App = () => {
   return (
     <Container>
       <Form onSearch={(value) => onSearch(value)} options={options} />
-      <Map type={type} coordinates={coords} />
+      <Map 
+        type={type}
+        coordinates={coords}
+        center={center && Object.keys(center).length ? [center.y, center.x] : []} />
     </Container>
   );
 }
